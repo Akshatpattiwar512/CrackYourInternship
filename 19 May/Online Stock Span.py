3b6fe41985996c0a9471bfee7912fdcfce1ab3bb
Akshat Pattiwar <akshatpattiwar512@gmail.com>
2022-05-19 10:45:21 +0530

    Update Online Stock Span.py

--- 19 May/Online Stock Span.py
+++ 19 May/Online Stock Span.py
@@ -28,6 +28,8 @@ class StockSpanner:
             #print(i, val)
         return self.res[-1]		
 
+'''
 # Your StockSpanner object will be instantiated and called as such:
 # obj = StockSpanner()
 # param_1 = obj.next(price)
+'''
