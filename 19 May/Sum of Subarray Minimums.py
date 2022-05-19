66c4068625ee9a2e51992d7113b2c654b6373b0f
Akshat Pattiwar <akshatpattiwar512@gmail.com>
2022-05-19 10:54:41 +0530

    Create Sum of Subarray Minimums.py

---
+++ 19 May/Sum of Subarray Minimums.py
@@ -0,0 +1,14 @@
+#Given an array of integers arr,find the sum of min(b),where b ranges over every(contiguous) subarray of arr.Since the answer may be large,return the answer modulo 109+ 7
+
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        stack = [-1]
+        result = 0
+        for idx, x in enumerate(arr + [0]):
+            while len(stack) > 1 and x < arr[stack[-1]]:
+                index = stack.pop()
+                left_length = index - stack[-1]
+                right_length = idx - index
+                result += left_length * right_length * arr[index]
+            stack.append(idx)
+        return result % (10 ** 9 + 7)
